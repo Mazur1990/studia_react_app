3f2619ba102623d0efcf830ac0bb240b7bfad6b0
Mazur1990 <andrzej.webyou@gmail.com>
2020-05-03 22:48:55 +0200

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div class="row">
                 <div class="col d-flex justify-content-center trening_container">
                     {/* <img src="./images/treningi/4831.jpg" alt=""></img> */}
-                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <h2>Lorem ipsum dolor </h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae modi fugit, sint velit distinctio alias cumque itaque veritatis harum quibusdam eveniet corrupti unde ab illum ipsam ad voluptatibus molestias odit.</p>
                     <button class="btn">Więcej</button>
                 </div>
